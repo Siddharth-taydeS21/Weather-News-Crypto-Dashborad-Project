f083b7b7eecc962ba0495fa1a1088e4d5753e2b1
Siddharth Tayde <taydesiddharth999@gmail.com>
2026-05-03 17:00:28 +0530

    Add waether & news section

---
+++ assets/JS/main.js
@@ -0,0 +1,19 @@
+//  ====== SHOW MOBILE NAV MENU ON MENU CLICK ======= 
+const toggle = document.getElementById('toggle_button');
+const nav_menu = document.querySelector('.nav_menu');
+
+const toggleLeft = () => {
+    nav_menu.classList.toggle('max-lg:left-[120%]');
+    nav_menu.classList.toggle('max-lg:left-0');
+};
+
+toggle.addEventListener('click', toggleLeft)
+
+// ====== HIDE MOBILE NAV MENU ======= 
+const closeBtn = document.getElementById('close_button');
+const nav_links = document.querySelectorAll('.nav_link');
+
+nav_links.forEach(link => {
+    link.addEventListener('click', toggleLeft)
+})
+closeBtn.addEventListener('click', toggleLeft)
